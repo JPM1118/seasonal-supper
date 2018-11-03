5b7892d2b9e21486bd5e1c69ab36fa214fb17fe6
Jack <johnmccormick1118@gmail.com>
2018-11-03 12:46:06 -0500

    First commit. Landing page and title block.

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from "./Title";
+
+export default LandingPage => {
+  return (
+    <div className="landing-page">
+      <Title />
+    </div>
+  );
+};
---
+++ src/Components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default Title => {
+  return (
+    <div className="title-block">
+      <h1 className="title-block--title">SEASONAL SUPPER</h1>
+      <h2 className="title-block--subtitle">Recipes for seasonal eating.</h2>
+    </div>
+  );
+};
